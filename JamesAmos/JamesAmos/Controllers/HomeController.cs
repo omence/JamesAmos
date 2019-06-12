3b819b773bcb0e62d294b02e658e669d5216fda5
Jason Few <omence11@gmail.com>
2019-06-12 15:24:02 -0700

    Some content and styling comleted, site is fully responsive.

--- JamesAmos/JamesAmos/Controllers/HomeController.cs
+++ JamesAmos/JamesAmos/Controllers/HomeController.cs
@@ -2,14 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JamesAmos.Data;
+using JamesAmos.Models.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JamesAmos.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IEmail _emailService;
+
+        private readonly JamesAmosDbContext _context;
+
+        public HomeController(JamesAmosDbContext context, IEmail emailService)
+        {
+            _context = context;
+
+            _emailService = emailService;
+
+        }
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult Vlog()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Contact(string email, string subject, string message)
+        {
+            if (email != null && subject != null && message != null)
+            {
+                await _emailService.SendEmail(email, subject, message);
+
+                TempData["Message"] = "Sent, we will get back to you ASAP";
+
+                return View();
+            }
+
             return View();
         }
     }
